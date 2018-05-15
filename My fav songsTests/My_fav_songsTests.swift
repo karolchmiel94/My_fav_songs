2544cc748d3e872e152ab29f3297365463d8110e
Karol Ch <karolch@MacBook-Pro-Macbook.local>
2018-05-15 22:15:42 +0200

    Initial commit

---
+++ My fav songsTests/My_fav_songsTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_fav_songsTests.swift
+//  My fav songsTests
+//
+//  Created by Karol Ch on 14/05/2018.
+//  Copyright © 2018 Karol Chmiel. All rights reserved.
+//
+
+import XCTest
+@testable import My_fav_songs
+
+class My_fav_songsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
